168263af808971c1045c7cbed56afc3a3596c8fb
FubyCoder <fabio.cavallari.01@outlook.it>
2021-02-23 19:42:40 +0100

    update

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import DefaultModal from 'react-modal';
+
+interface ModalProp {
+    className?: string;
+    visible: boolean;
+    onCloseRequest: () => void;
+    children?: JSX.Element;
+}
+
+function Modal({ className = '', visible, onCloseRequest, children }: ModalProp) {
+    DefaultModal.setAppElement('#app-root');
+
+    return (
+        <DefaultModal
+            isOpen={visible}
+            onRequestClose={onCloseRequest}
+            shouldFocusAfterRender={false}
+            htmlOpenClassName="ReactModal__Body--open"
+            overlayClassName={'modal'}
+            className={'modal-card ' + className}
+        >
+            {children}
+        </DefaultModal>
+    );
+}
+
+export default Modal;
--- src/pages/TodoApi/index.tsx
+++ src/pages/TodoApi/index.tsx
@@ -5,23 +5,40 @@ import { ITask, TodoI } from '../../type';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { request } from '../../utils';
+import Modal from '../../components/Modal';
 
 type FormElement = React.FormEvent<HTMLFormElement>;
 
 function Todo() {
+    // Page State
     const [newTask, setNewTask] = useState<string>('');
     const [tasks, setTasks] = useState<TodoI[]>([]);
+    const [sharedTasks, setSharedTasks] = useState<TodoI[]>([]);
     const [reload, setReload] = useState(false);
 
     const taskInput = useRef<HTMLInputElement>(null);
+
+    // Modal State
+    const [showModal, setShowModal] = useState(false);
+    const [selectedTodo, setSelectedTodo] = useState<TodoI>();
+    const [modalEmail, setModalEmail] = useState('');
+
+    const onAddUserToTodo = () => {
+        request('post', '/todo/share/', { _id: selectedTodo?._id, email: modalEmail })
+            .then(() => {
+                setShowModal(false);
+                setSelectedTodo(undefined);
+                setModalEmail('');
+                setReload(!reload);
+            })
+            .catch(err => {});
+    };
 
     const handleSubmit = (e: FormElement) => {
         e.preventDefault();
 
         request('post', '/todo/create', { title: newTask })
-            .then(data => {
-                setReload(!reload);
-            })
+            .then(data => setReload(!reload))
             .catch(err => {});
     };
 
@@ -35,9 +52,11 @@ function Todo() {
 
     useEffect(() => {
         request('get', '/todo/list')
-            .then(data => {
-                setTasks(data);
-            })
+            .then(data => setTasks(data))
+            .catch(() => {});
+
+        request('get', '/todo/share/list')
+            .then(data => setSharedTasks(data))
             .catch(() => {});
     }, [reload]);
 
@@ -50,7 +69,14 @@ function Todo() {
                             <div className="card-content is-rounded">
                                 <p className="title is-3 has-text-centered">Task Form</p>
                                 <form onSubmit={handleSubmit}>
-                                    <input className="has-text-grey-dark input is-rounded" type="text" onChange={e => setNewTask(e.target.value)} value={newTask} ref={taskInput} autoFocus></input>
+                                    <input
+                                        className="has-text-grey-dark input is-rounded border"
+                                        type="text"
+                                        onChange={e => setNewTask(e.target.value)}
+                                        value={newTask}
+                                        ref={taskInput}
+                                        autoFocus
+                                    ></input>
                                     <br />
                                     <br />
                                     <div className="buttons is-right">
@@ -60,28 +86,62 @@ function Todo() {
                             </div>
                         </div>
                         <br />
-                        <div className="w-1/2 mx-auto">
-                            {tasks.map(task => {
-                                return (
-                                    <div className="flex flex-row items-center border justify-between">
-                                        <p className="px-5" key={task._id}>
-                                            {task.title}
-                                        </p>
-                                        <div>
-                                            <Link to={`/api/` + task._id} className="bg-green-400 hover:bg-green-600 px-5 py-2" onClick={() => {}}>
-                                                Modifica
-                                            </Link>
-                                            <button className="bg-red-400 hover:bg-red-600 px-5 py-2" onClick={() => onDeleteClick(task._id)}>
-                                                Elimina
-                                            </button>
+                        <div className="flex flex-row w-full">
+                            <div className="w-1/2 px-2">
+                                <h1 className="mb-5">Todo Personali</h1>
+                                {tasks.map(task => {
+                                    return (
+                                        <div className="flex flex-row items-center border justify-between">
+                                            <p className="px-5" key={task._id}>
+                                                {task.title}
+                                            </p>
+                                            <div>
+                                                <button
+                                                    className="bg-blue-400 hover:bg-blue-600 px-5 py-2"
+                                                    onClick={() => {
+                                                        setSelectedTodo(task);
+                                                        setShowModal(true);
+                                                    }}
+                                                >
+                                                    Condividi
+                                                </button>
+                                                <Link to={`/api/` + task._id} className="bg-green-400 hover:bg-green-600 px-5 py-2 h-full">
+                                                    Modifica
+                                                </Link>
+                                                <button className="bg-red-400 hover:bg-red-600 px-5 py-2" onClick={() => onDeleteClick(task._id)}>
+                                                    Elimina
+                                                </button>
+                                            </div>
                                         </div>
-                                    </div>
-                                );
-                            })}
+                                    );
+                                })}
+                            </div>
+                            <div className="w-1/2 px-2">
+                                <h1 className="mb-5">Todo Condivise</h1>
+                                <div>
+                                    {sharedTasks.map(task => {
+                                        return (
+                                            <div className="flex flex-row items-center border justify-between">
+                                                <p className="px-5 py-2" key={task._id}>
+                                                    {task.title}
+                                                </p>
+                                            </div>
+                                        );
+                                    })}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            <Modal visible={showModal} onCloseRequest={() => setShowModal(false)}>
+                <div className="bg-white w-full h-60 p-10 rounded-md">
+                    <p>Inserisci Email</p>
+                    <input type="email" className="border" value={modalEmail} onChange={e => setModalEmail(e.target.value)} />
+                    <button onClick={onAddUserToTodo}>Aggiungi</button>
+                </div>
+            </Modal>
         </>
     );
 }
